854d37cec983692c81311abf6a2e151cbec05fb2
Dmitry <mitya.shabat@gmail.com>
2024-01-22 15:55:25 +0400

    Add transliteration to handle russian characters

--- _plugins/recipe-collector.rb
+++ _plugins/recipe-collector.rb
@@ -9,6 +9,44 @@ module RecipeCollectorModule
       end
       @@recipe_data
     end
+    def self.recipe_name_to_url(recipe_name)
+      ru_to_en = {
+        "а" => "a",
+        "б" => "b",
+        "в" => "v",
+        "г" => "g",
+        "д" => "d",
+        "е" => "e",
+        "ё" => "yo",
+        "ж" => "zh",
+        "з" => "z",
+        "и" => "i",
+        "й" => "j",
+        "к" => "k",
+        "л" => "l",
+        "м" => "m",
+        "н" => "n",
+        "о" => "o",
+        "п" => "p",
+        "р" => "r",
+        "с" => "s",
+        "т" => "t",
+        "у" => "u",
+        "ф" => "f",
+        "х" => "h",
+        "ц" => "c",
+        "ч" => "ch",
+        "ш" => "sh",
+        "щ" => "shh",
+        "ъ" => "",
+        "ы" => "y",
+        "ь" => "",
+        "э" => "e",
+        "ю" => "yu",
+        "я" => "ya",
+      }
+      recipe_name.strip.downcase.gsub(' ', '-').gsub(/[а-я]/, ru_to_en)
+    end
   end
 
   class RecipeCollectorNamesTag < Liquid::Tag
@@ -23,7 +61,7 @@ module RecipeCollectorModule
       # Get recipe data
       recipe_data = RecipeCollectorWrapper.get_recipe_data(notion_api_key)
       # Prepend each name with a dash
-      recipe_names = recipe_data.map { |recipe| "- <a href=\"#{recipe['name'].strip.downcase.gsub(' ', '-')}\">#{recipe['name'].strip}</a>\n\n" }
+      recipe_names = recipe_data.map { |recipe| "- <a href=\"#{RecipeCollectorWrapper.recipe_name_to_url(recipe['name'])}\">#{recipe['name'].strip}</a>\n\n" }
       # Join the array into a string
       recipe_names.join("\n")
     end
@@ -42,7 +80,7 @@ module RecipeCollectorModule
     def initialize(site, base, recipe_name, recipe_text, recipe_url)
       @site = site
       @base = base
-      @dir = "recipes/#{recipe_name.strip.downcase.gsub(' ', '-')}"
+      @dir = "recipes/#{RecipeCollectorWrapper.recipe_name_to_url(recipe_name)}"
 
       @basename = 'index'
       @ext = '.html'
